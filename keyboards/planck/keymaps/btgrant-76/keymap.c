1a9f179052ca8012305b425886546e40645235a2
Brian Grant <brian@skeletonjack.net>
2023-02-07 14:39:01 -0600

    fix Planck build

--- keyboards/planck/keymaps/btgrant-76/keymap.c
+++ keyboards/planck/keymaps/btgrant-76/keymap.c
@@ -182,7 +182,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
