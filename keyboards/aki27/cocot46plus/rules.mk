245f5ca980ba84cba192a81c71326c8b40e72f4b
Brian Grant <brian@skeletonjack.net>
2023-09-12 19:42:06 -0500

    upgrade to 0.22.3

--- keyboards/aki27/cocot46plus/rules.mk
+++ keyboards/aki27/cocot46plus/rules.mk
@@ -27,7 +27,6 @@ POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = adns5050
 
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
 
 # Disable unnecessary functions to reduce firmware size
 SPACE_CADET_ENABLE = no
--- keyboards/crkbd/keymaps/btgrant-76/rules.mk
+++ keyboards/crkbd/keymaps/btgrant-76/rules.mk
@@ -2,6 +2,5 @@ RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE = yes
 VIA_ENABLE      = no    # 2278 bytes
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
 
 CONVERT_TO = blok
