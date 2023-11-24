btgrant-76/qmk_firmware

10-column layout & Planck QAZ

users/btgrant-76/btgrant-76.h
@@ -169,63 +169,104 @@ enum {
 #define ___HRM_R___        KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL
 
 // base
-#define ___BASE_1_L___     TAB,     KC_Q,   KC_W,   KC_E,    KC_R,   KC_T
-#define ___BASE_2_L___     ESC,     A_CTL,  S_ALT,  D_GUI,   F_SFT,  KC_G
-#define ___BASE_3_L___     L_SFT,   Z_MEH,  KC_X,   KC_C,    KC_V,   KC_B
-#define ___BASE_THUMB_L___                          TAB_FUN, BS_NUM, ESC_SYM
+#define ___5BASE_1_L___     KC_Q,   KC_W,   KC_E,    KC_R,   KC_T
+#define ___5BASE_2_L___     A_CTL,  S_ALT,  D_GUI,   F_SFT,  KC_G
+#define ___5BASE_3_L___     Z_MEH,  KC_X,   KC_C,    KC_V,   KC_B
+#define ___BASE_THUMB_L___                  TAB_FUN, BS_NUM, ESC_SYM
 
-#define ___BASE_1_R___     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     BSPC
-#define ___BASE_2_R___     KC_H,    J_SFT,   K_GUI,   L_ALT,  SCLN_CTL, QUOT
-#define ___BASE_3_R___     KC_N,    KC_M,    KC_COMM, KC_DOT, SLSH_ALL, ENT
-#define ___BASE_THUMB_R___ ENT_MED, SPC_NAV, DEL_MOUS
+#define ___BASE_1_L___      TAB,     ___5BASE_1_L___
+#define ___BASE_2_L___      ESC,     ___5BASE_2_L___
+#define ___BASE_3_L___      L_SFT,   ___5BASE_3_L___
+
+#define ___5BASE_1_R___     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P
+#define ___5BASE_2_R___     KC_H,    J_SFT,   K_GUI,   L_ALT,  SCLN_CTL
+#define ___5BASE_3_R___     KC_N,    KC_M,    KC_COMM, KC_DOT, SLSH_ALL
+#define ___BASE_THUMB_R___  ENT_MED, SPC_NAV, DEL_MOUS
+
+#define ___BASE_1_R___     ___5BASE_1_R___, BSPC
+#define ___BASE_2_R___     ___5BASE_2_R___, QUOT
+#define ___BASE_3_R___     ___5BASE_3_R___, ENT
 
 // symbol
-#define ___SYM_1_L___     XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ___SYM_2_L___     KC_CAPS, ___HRM_L___,                            XXXXXXX
-#define ___SYM_3_L___     _______, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ___SYM_THUMB_L___                                XXXXXXX, XXXXXXX, XXXXXXX
+#define ___5SYM_1_L___     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ___5SYM_2_L___     ___HRM_L___,                            XXXXXXX
+#define ___5SYM_3_L___     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ___SYM_THUMB_L___                        XXXXXXX, XXXXXXX, XXXXXXX
 
-#define ___SYM_1_R___     KC_LCBR, KC_AMPR, KC_ASTR, LPRN_TD, RCBR_TD, KC_DEL
-#define ___SYM_2_R___     KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_DQT,  KC_TILD  // KC_QUOT
-#define ___SYM_3_R___     KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_TILD, XXXXXXX
-#define ___SYM_THUMB_R___ KC_UNDS, KC_LPRN, RPRN_TD
+#define ___SYM_1_L___     XXXXXXX, ___5SYM_1_L___
+#define ___SYM_2_L___     KC_CAPS, ___5SYM_2_L___
+#define ___SYM_3_L___     _______, ___5SYM_3_L___
+
+#define ___5SYM_1_R___     KC_LCBR, KC_AMPR, KC_ASTR, LPRN_TD, RCBR_TD
+#define ___5SYM_2_R___     KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_DQT
+#define ___5SYM_3_R___     KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_TILD
+#define ___SYM_THUMB_R___  KC_UNDS, KC_LPRN, RPRN_TD
+
+#define ___SYM_1_R___      ___5SYM_1_R___, KC_DEL
+#define ___SYM_2_R___      ___5SYM_2_R___, KC_TILD
+#define ___SYM_3_R___      ___5SYM_3_R___, XXXXXXX
 
 // number
-#define ___NUM_1_L___     XXXXXXX, KC_MPRV,     KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX
-#define ___NUM_2_L___     XXXXXXX, ___HRM_L___,                            RGB_SPI
-#define ___NUM_3_L___     _______, RGB_TOG,     RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI
-#define ___NUM_THUMB_L___                            KC_MUTE, KC_MPLY, KC_MSTP
+#define ___5NUM_1_L___     KC_MPRV,     KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX
+#define ___5NUM_2_L___     ___HRM_L___,                            RGB_SPI
+#define ___5NUM_3_L___     RGB_TOG,     RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI
+#define ___NUM_THUMB_L___                        KC_MUTE, KC_MPLY, KC_MSTP
 
-#define ___NUM_1_R___     KC_LBRC, KC_7, KC_8,  KC_9, RBRC_TD, KC_BSPC
-#define ___NUM_2_R___     KC_EQL,  KC_4, KC_5,  KC_6, KC_QUOT, KC_PENT // KC_DQT
-#define ___NUM_3_R___     KC_BSLS, KC_1, KC_2,  KC_3, GRAV_TD, _______
+#define ___NUM_1_L___     XXXXXXX, ___5NUM_1_L___
+#define ___NUM_2_L___     XXXXXXX, ___5NUM_2_L___
+#define ___NUM_3_L___     _______, ___5NUM_3_L___
+
+#define ___5NUM_1_R___    KC_LBRC, KC_7, KC_8,  KC_9, RBRC_TD
+#define ___5NUM_2_R___    KC_EQL,  KC_4, KC_5,  KC_6, KC_QUOT
+#define ___5NUM_3_R___    KC_BSLS, KC_1, KC_2,  KC_3, GRAV_TD
 #define ___NUM_THUMB_R___ KC_MINS, KC_0, KC_DOT
 
-// navigation
-#define ___NAV_1_L___     XXXXXXX, TAB_LFT, BACK,    FWD,     TAB_RGT, UP_DIR
-#define ___NAV_2_L___     XXXXXXX, ___HRM_L___,                        XXXXXXX
-#define ___NAV_3_L___     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ___NAV_THUMB_L___                            DEL_GUI, KC_BSPC, KC_ENT
+#define ___NUM_1_R___     ___5NUM_1_R___, KC_BSPC
+#define ___NUM_2_R___     ___5NUM_2_R___, KC_PENT
+#define ___NUM_3_R___     ___5NUM_3_R___, _______
 
-#define ___NAV_1_R___     REDO,    PASTE,   COPY,    CUT,      UNDO,    XXXXXXX
-#define ___NAV_2_R___     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_CAPS, XXXXXXX
-#define ___NAV_3_R___     KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_INS,  _______
+// navigation
+#define ___5NAV_1_L___    TAB_LFT, BACK,    FWD,     TAB_RGT, UP_DIR
+#define ___5NAV_2_L___    ___HRM_L___,                        XXXXXXX
+#define ___5NAV_3_L___    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ___NAV_THUMB_L___                   DEL_GUI, KC_BSPC, KC_ENT
+
+#define ___NAV_1_L___     XXXXXXX, ___5NAV_1_L___
+#define ___NAV_2_L___     XXXXXXX, ___5NAV_2_L___
+#define ___NAV_3_L___     _______, ___5NAV_3_L___
+
+#define ___5NAV_1_R___    REDO,    PASTE,   COPY,    CUT,      UNDO
+#define ___5NAV_2_R___    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_CAPS
+#define ___5NAV_3_R___    KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_INS
 #define ___NAV_THUMB_R___ XXXXXXX, XXXXXXX, XXXXXXX
 
-// function
-#define ___FUN_1_L___     XXXXXXX, XXXXXXX, UP_DIR,  SCRN2CLP, SCRN2FL, XXXXXXX
-#define ___FUN_2_L___     LOG_OUT, XXXXXXX, DT_PRNT, DT_UP,    DT_DOWN
-#define ___FUN_3_L___     _______, XXXXXXX, AS_RPT,  AS_UP,    AS_DOWN, XXXXXXX
-#define ___FUN_THUMB_L___                            XXXXXXX,  KC_SPC,  KC_TAB
+#define ___NAV_1_R___     ___5NAV_1_R___, XXXXXXX
+#define ___NAV_2_R___     ___5NAV_2_R___, XXXXXXX
+#define ___NAV_3_R___     ___5NAV_3_R___, _______
 
-#define ___FUN_1_R___     XXXXXXX, KC_F7,   KC_F8,   F9_TD, F12_TD, XXXXXXX
-#define ___FUN_2_R___     XXXXXXX, KC_F4,   KC_F5,   F6_TD, KC_F11, XXXXXXX
-#define ___FUN_3_R___     XXXXXXX, F1_TD,   F2_TD,   KC_F3, KC_F10, XXXXXXX
+// function
+#define ___5FUN_1_L___    XXXXXXX, UP_DIR,  SCRN2CLP, SCRN2FL, XXXXXXX
+#define ___5FUN_2_L___    XXXXXXX, DT_PRNT, DT_UP,    DT_DOWN
+#define ___5FUN_3_L___    XXXXXXX, AS_RPT,  AS_UP,    AS_DOWN, XXXXXXX
+#define ___FUN_THUMB_L___                   XXXXXXX,  KC_SPC,  KC_TAB
+
+#define ___FUN_1_L___     XXXXXXX, ___5FUN_1_L___
+#define ___FUN_2_L___     LOG_OUT, ___5FUN_2_L___
+#define ___FUN_3_L___     _______, ___5FUN_3_L___
+
+#define ___5FUN_1_R___    XXXXXXX, KC_F7,   KC_F8,   F9_TD, F12_TD
+#define ___5FUN_2_R___    XXXXXXX, KC_F4,   KC_F5,   F6_TD, KC_F11
+#define ___5FUN_3_R___    XXXXXXX, F1_TD,   F2_TD,   KC_F3, KC_F10
 #define ___FUN_THUMB_R___ XXXXXXX, XXXXXXX, XXXXXXX
 
+#define ___FUN_1_R___     ___5FUN_1_R___, XXXXXXX
+#define ___FUN_2_R___     ___5FUN_2_R___, XXXXXXX
+#define ___FUN_3_R___     ___5FUN_3_R___, XXXXXXX
+
 // layout macros
+#define LAYOUT_btgrant(...)     LAYOUT(__VA_ARGS__)
 #define LAYOUT_btgrant_3x6(...) LAYOUT_split_3x6_3(__VA_ARGS__)
-#define LAYOUT_btgrant(...) LAYOUT(__VA_ARGS__)
+#define LAYOUT_btgrant_grid(...) LAYOUT_planck_grid(__VA_ARGS__)
 // portable keymaps end
 
 #endif

btgrant-76/qmk_userspace

users/btgrant-76/btgrant-76.h
@@ -169,63 +169,104 @@ enum {
 #define ___HRM_R___        KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL
 
 // base
-#define ___BASE_1_L___     TAB,     KC_Q,   KC_W,   KC_E,    KC_R,   KC_T
-#define ___BASE_2_L___     ESC,     A_CTL,  S_ALT,  D_GUI,   F_SFT,  KC_G
-#define ___BASE_3_L___     L_SFT,   Z_MEH,  KC_X,   KC_C,    KC_V,   KC_B
-#define ___BASE_THUMB_L___                          TAB_FUN, BS_NUM, ESC_SYM
+#define ___5BASE_1_L___     KC_Q,   KC_W,   KC_E,    KC_R,   KC_T
+#define ___5BASE_2_L___     A_CTL,  S_ALT,  D_GUI,   F_SFT,  KC_G
+#define ___5BASE_3_L___     Z_MEH,  KC_X,   KC_C,    KC_V,   KC_B
+#define ___BASE_THUMB_L___                  TAB_FUN, BS_NUM, ESC_SYM
 
-#define ___BASE_1_R___     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     BSPC
-#define ___BASE_2_R___     KC_H,    J_SFT,   K_GUI,   L_ALT,  SCLN_CTL, QUOT
-#define ___BASE_3_R___     KC_N,    KC_M,    KC_COMM, KC_DOT, SLSH_ALL, ENT
-#define ___BASE_THUMB_R___ ENT_MED, SPC_NAV, DEL_MOUS
+#define ___BASE_1_L___      TAB,     ___5BASE_1_L___
+#define ___BASE_2_L___      ESC,     ___5BASE_2_L___
+#define ___BASE_3_L___      L_SFT,   ___5BASE_3_L___
+
+#define ___5BASE_1_R___     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P
+#define ___5BASE_2_R___     KC_H,    J_SFT,   K_GUI,   L_ALT,  SCLN_CTL
+#define ___5BASE_3_R___     KC_N,    KC_M,    KC_COMM, KC_DOT, SLSH_ALL
+#define ___BASE_THUMB_R___  ENT_MED, SPC_NAV, DEL_MOUS
+
+#define ___BASE_1_R___     ___5BASE_1_R___, BSPC
+#define ___BASE_2_R___     ___5BASE_2_R___, QUOT
+#define ___BASE_3_R___     ___5BASE_3_R___, ENT_SFT
 
 // symbol
-#define ___SYM_1_L___     XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ___SYM_2_L___     KC_CAPS, ___HRM_L___,                            XXXXXXX
-#define ___SYM_3_L___     _______, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ___SYM_THUMB_L___                                XXXXXXX, XXXXXXX, XXXXXXX
+#define ___5SYM_1_L___     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ___5SYM_2_L___     ___HRM_L___,                            XXXXXXX
+#define ___5SYM_3_L___     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ___SYM_THUMB_L___                        XXXXXXX, XXXXXXX, XXXXXXX
 
-#define ___SYM_1_R___     KC_LCBR, KC_AMPR, KC_ASTR, LPRN_TD, RCBR_TD, KC_DEL
-#define ___SYM_2_R___     KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_DQT,  KC_TILD  // KC_QUOT
-#define ___SYM_3_R___     KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_TILD, XXXXXXX
-#define ___SYM_THUMB_R___ KC_UNDS, KC_LPRN, RPRN_TD
+#define ___SYM_1_L___     XXXXXXX, ___5SYM_1_L___
+#define ___SYM_2_L___     KC_CAPS, ___5SYM_2_L___
+#define ___SYM_3_L___     _______, ___5SYM_3_L___
+
+#define ___5SYM_1_R___     KC_LCBR, KC_AMPR, KC_ASTR, LPRN_TD, RCBR_TD
+#define ___5SYM_2_R___     KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_DQT
+#define ___5SYM_3_R___     KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_TILD
+#define ___SYM_THUMB_R___  KC_UNDS, KC_LPRN, RPRN_TD
+
+#define ___SYM_1_R___      ___5SYM_1_R___, KC_DEL
+#define ___SYM_2_R___      ___5SYM_2_R___, KC_TILD
+#define ___SYM_3_R___      ___5SYM_3_R___, XXXXXXX
 
 // number
-#define ___NUM_1_L___     XXXXXXX, KC_MPRV,     KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX
-#define ___NUM_2_L___     XXXXXXX, ___HRM_L___,                            RGB_SPI
-#define ___NUM_3_L___     _______, RGB_TOG,     RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI
-#define ___NUM_THUMB_L___                            KC_MUTE, KC_MPLY, KC_MSTP
+#define ___5NUM_1_L___     KC_MPRV,     KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX
+#define ___5NUM_2_L___     ___HRM_L___,                            RGB_SPI
+#define ___5NUM_3_L___     RGB_TOG,     RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI
+#define ___NUM_THUMB_L___                        KC_MUTE, KC_MPLY, KC_MSTP
 
-#define ___NUM_1_R___     KC_LBRC, KC_7, KC_8,  KC_9, RBRC_TD, KC_BSPC
-#define ___NUM_2_R___     KC_EQL,  KC_4, KC_5,  KC_6, KC_QUOT, KC_PENT // KC_DQT
-#define ___NUM_3_R___     KC_BSLS, KC_1, KC_2,  KC_3, GRAV_TD, _______
+#define ___NUM_1_L___     XXXXXXX, ___5NUM_1_L___
+#define ___NUM_2_L___     XXXXXXX, ___5NUM_2_L___
+#define ___NUM_3_L___     _______, ___5NUM_3_L___
+
+#define ___5NUM_1_R___    KC_LBRC, KC_7, KC_8,  KC_9, RBRC_TD
+#define ___5NUM_2_R___    KC_EQL,  KC_4, KC_5,  KC_6, KC_QUOT
+#define ___5NUM_3_R___    KC_BSLS, KC_1, KC_2,  KC_3, GRAV_TD
 #define ___NUM_THUMB_R___ KC_MINS, KC_0, KC_DOT
 
-// navigation
-#define ___NAV_1_L___     XXXXXXX, TAB_LFT, BACK,    FWD,     TAB_RGT, UP_DIR
-#define ___NAV_2_L___     XXXXXXX, ___HRM_L___,                        XXXXXXX
-#define ___NAV_3_L___     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ___NAV_THUMB_L___                            DEL_GUI, KC_BSPC, KC_ENT
+#define ___NUM_1_R___     ___5NUM_1_R___, KC_BSPC
+#define ___NUM_2_R___     ___5NUM_2_R___, KC_PENT
+#define ___NUM_3_R___     ___5NUM_3_R___, _______
 
-#define ___NAV_1_R___     REDO,    PASTE,   COPY,    CUT,      UNDO,    XXXXXXX
-#define ___NAV_2_R___     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_CAPS, XXXXXXX
-#define ___NAV_3_R___     KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_INS,  _______
+// navigation
+#define ___5NAV_1_L___    TAB_LFT, BACK,    FWD,     TAB_RGT, UP_DIR
+#define ___5NAV_2_L___    ___HRM_L___,                        XXXXXXX
+#define ___5NAV_3_L___    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ___NAV_THUMB_L___                   DEL_GUI, KC_BSPC, KC_ENT
+
+#define ___NAV_1_L___     XXXXXXX, ___5NAV_1_L___
+#define ___NAV_2_L___     XXXXXXX, ___5NAV_2_L___
+#define ___NAV_3_L___     _______, ___5NAV_3_L___
+
+#define ___5NAV_1_R___    REDO,    PASTE,   COPY,    CUT,      UNDO
+#define ___5NAV_2_R___    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_CAPS
+#define ___5NAV_3_R___    KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_INS
 #define ___NAV_THUMB_R___ XXXXXXX, XXXXXXX, XXXXXXX
 
-// function
-#define ___FUN_1_L___     XXXXXXX, XXXXXXX, UP_DIR,  SCRN2CLP, SCRN2FL, XXXXXXX
-#define ___FUN_2_L___     LOG_OUT, XXXXXXX, DT_PRNT, DT_UP,    DT_DOWN
-#define ___FUN_3_L___     _______, XXXXXXX, AS_RPT,  AS_UP,    AS_DOWN, XXXXXXX
-#define ___FUN_THUMB_L___                            XXXXXXX,  KC_SPC,  KC_TAB
+#define ___NAV_1_R___     ___5NAV_1_R___, XXXXXXX
+#define ___NAV_2_R___     ___5NAV_2_R___, XXXXXXX
+#define ___NAV_3_R___     ___5NAV_3_R___, _______
 
-#define ___FUN_1_R___     XXXXXXX, KC_F7,   KC_F8,   F9_TD, F12_TD, XXXXXXX
-#define ___FUN_2_R___     XXXXXXX, KC_F4,   KC_F5,   F6_TD, KC_F11, XXXXXXX
-#define ___FUN_3_R___     XXXXXXX, F1_TD,   F2_TD,   KC_F3, KC_F10, XXXXXXX
+// function
+#define ___5FUN_1_L___    XXXXXXX, UP_DIR,  SCRN2CLP, SCRN2FL, XXXXXXX
+#define ___5FUN_2_L___    XXXXXXX, DT_PRNT, DT_UP,    DT_DOWN
+#define ___5FUN_3_L___    XXXXXXX, AS_RPT,  AS_UP,    AS_DOWN, XXXXXXX
+#define ___FUN_THUMB_L___                   XXXXXXX,  KC_SPC,  KC_TAB
+
+#define ___FUN_1_L___     XXXXXXX, ___5FUN_1_L___
+#define ___FUN_2_L___     LOG_OUT, ___5FUN_2_L___
+#define ___FUN_3_L___     _______, ___5FUN_3_L___
+
+#define ___5FUN_1_R___    XXXXXXX, KC_F7,   KC_F8,   F9_TD, F12_TD
+#define ___5FUN_2_R___    XXXXXXX, KC_F4,   KC_F5,   F6_TD, KC_F11
+#define ___5FUN_3_R___    XXXXXXX, F1_TD,   F2_TD,   KC_F3, KC_F10
 #define ___FUN_THUMB_R___ XXXXXXX, XXXXXXX, XXXXXXX
 
+#define ___FUN_1_R___     ___5FUN_1_R___, XXXXXXX
+#define ___FUN_2_R___     ___5FUN_2_R___, XXXXXXX
+#define ___FUN_3_R___     ___5FUN_3_R___, XXXXXXX
+
 // layout macros
+#define LAYOUT_btgrant(...)     LAYOUT(__VA_ARGS__)
 #define LAYOUT_btgrant_3x6(...) LAYOUT_split_3x6_3(__VA_ARGS__)
-#define LAYOUT_btgrant(...) LAYOUT(__VA_ARGS__)
+#define LAYOUT_btgrant_grid(...) LAYOUT_planck_grid(__VA_ARGS__)
 // portable keymaps end
 
 #endif