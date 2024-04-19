d0253c683e275ab2151473c4e50f6c957e1600d8
Brian Grant <brian@skeletonjack.net>
2024-04-19 16:45:30 -0500

    vim-friendly esc combo

--- users/btgrant-76/combos.h
+++ users/btgrant-76/combos.h
@@ -1,6 +1,6 @@
 const uint16_t PROGMEM QUOT[] = {L_ALT, SCLN_CTL, COMBO_END};
 const uint16_t PROGMEM D_QUOT[] = {LSFT_T(KC_F), RALT_T(KC_L), RCTL_T(KC_SCLN), COMBO_END};
-const uint16_t PROGMEM ESC_TOP[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM ESC_TOP[] = {KC_Q, KC_E, COMBO_END};
 const uint16_t PROGMEM ESC_BOT[] = {Z_HYPR, KC_X, COMBO_END};
 const uint16_t PROGMEM ENTER[] = {HYPR_T(KC_SLSH), KC_DOT, COMBO_END};
 
