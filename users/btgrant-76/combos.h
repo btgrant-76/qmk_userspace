6534cae6ddc367b7d23f443dd691038a1ed36bb1
Brian <brian@skeletonjack.net>
2024-06-09 10:01:29 -0500

    new combos and enable combos on cocot46plus

--- users/btgrant-76/combos.h
+++ users/btgrant-76/combos.h
@@ -2,12 +2,16 @@ const uint16_t PROGMEM QUOT[] = {L_ALT, SCLN_CTL, COMBO_END};
 const uint16_t PROGMEM D_QUOT[] = {LSFT_T(KC_F), RALT_T(KC_L), RCTL_T(KC_SCLN), COMBO_END};
 const uint16_t PROGMEM ESC_TOP[] = {KC_Q, KC_E, COMBO_END};
 const uint16_t PROGMEM ESC_BOT[] = {Z_HYPR, KC_X, COMBO_END};
+const uint16_t PROGMEM ESC_BOT2[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM ENTER[] = {HYPR_T(KC_SLSH), KC_DOT, COMBO_END};
+const uint16_t PROGMEM ENTER2[] = {KC_M, KC_COMM, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(QUOT, KC_QUOT),
     COMBO(D_QUOT, KC_DQT),
     COMBO(ESC_TOP, KC_ESC),
     COMBO(ESC_BOT, KC_ESC),
-    COMBO(ENTER, KC_ENT)
+    COMBO(ESC_BOT2, KC_ESC),
+    COMBO(ENTER, KC_ENT),
+    COMBO(ENTER2, KC_ENT)
 };
