6e02c4bbb1cb80ac9dcbdc453c467043802c5d6e
Brian <brian@skeletonjack.net>
2024-07-09 06:22:37 -0500

    copy/paste from Tominabox Le Chiffre

--- keyboards/sporkus/le_chiffre_32/keymaps/btgrant-76/keymap.c
+++ keyboards/sporkus/le_chiffre_32/keymaps/btgrant-76/keymap.c
@@ -1,38 +1,69 @@
 // Copyright 2023 sporkus
+// Copyright 2024 btgrant-76
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 #include "../../rgb.c"
+#include "btgrant-76.h"
 
-// Just something for quick testing, not meant to be used
+#ifdef COMBO_ENABLE
+#   include "combos.h"
+#endif
+
+#ifdef ENCODER_MAP_ENABLE
+#   include "encoder_map.h"
+#   include "encoder_press.h"
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,      RGB_TOG,     KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,
-         KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,    KC_L,   KC_QUOT,
-         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,
-                         LT(1, KC_ESC),  KC_ENT,                 KC_SPC, LT(2, KC_BSPC)
+    [_BASE] = LAYOUT_btgrant(
+        ___5BASE_1_L___,              ENC_BASE,                    ___5BASE_1_R___,
+        ___5BASE_2_L___,                                           ___5BASE_2_R___,
+        ___5BASE_3_L___,                                           ___5BASE_3_R___,
+        TAB_FUN, BS_NUM,                                          SPC_NAV, DEL_MED
     ),
-
-    [1] = LAYOUT(
-         _______,  _______,  _______,  _______,  _______,  QK_BOOT,  _______,  KC_7,     KC_8,     KC_9,     KC_0,
-         _______,  _______,  _______,  _______,  _______,            _______,  KC_4,     KC_5,     KC_6,     _______,
-         _______,  _______,  _______,  _______,  _______,            _______,  KC_1,     KC_2,     KC_3,     _______,
-                                       _______,  _______,            _______,  _______
+    [_NUM] = LAYOUT_btgrant(
+        ___5NUM_1_L___,                ENC_NUM,                    ___5NUM_1_R___,
+        ___5NUM_2_L___,                                            ___5NUM_2_R___,
+        ___5NUM_3_L___,                                            ___5NUM_3_R___,
+        XXXXXXX, XXXXXXX,                                        KC_MINS, ZERO_TD
     ),
-
-    [2] = LAYOUT(
-         _______,  _______,  _______,  _______,  _______,  QK_BOOT,  _______,  _______,   _______,   _______,    _______,
-         _______,  _______,  _______,  _______,  _______,            KC_LEFT,  KC_DOWN,   KC_UP,     KC_LEFT,    _______,
-         _______,  _______,  _______,  _______,  _______,            _______,  _______,   _______,   _______,    _______,
-                                       _______,  _______,            _______,  _______
+    [_SYM] = LAYOUT_btgrant(
+        ___5SYM_1_L___,                ENC_SYM,                    ___5SYM_1_R___,
+        ___5SYM_2_L___,                                            ___5SYM_2_R___,
+        ___5SYM_3_L___,                                            ___5SYM_3_R___,
+        KC_MUTE, KC_MPLY,                                       LPRN_MSE, XXXXXXX
+    ),
+    [_NAV] = LAYOUT_btgrant(
+        ___5NAV_1_L___,                ENC_NAV,                    ___5NAV_1_R___,
+        ___5NAV_2_L___,                                            ___5NAV_2_R___,
+        ___5NAV_3_L___,                                            ___5NAV_3_R___,
+        XXXXXXX, XXXXXXX,                                        XXXXXXX, XXXXXXX
+    ),
+    [_FUN] = LAYOUT_btgrant(
+        ___5FUN_1_L___,                ENC_FUN,                    ___5FUN_1_R___,
+        ___5FUN_2_L___,                                            ___5FUN_2_R___,
+        ___5FUN_3_L___,                                            ___5FUN_3_R___,
+        XXXXXXX, MO(_MSE),                                      MO(_ADD), XXXXXXX
+    ),
+    [_MSE] = LAYOUT_btgrant(
+        ___5MSE_1_L___,                ENC_MSE,                    ___5MSE_1_R___,
+        ___5MSE_2_L___,                                            ___5MSE_2_R___,
+        ___5MSE_3_L___,                                            ___5MSE_3_R___,
+        XXXXXXX, XXXXXXX,                                        XXXXXXX, XXXXXXX
+    ),
+    [_ADD] = LAYOUT_btgrant(
+        ___5ADD_1_L___,                ENC_ADD,                    ___5ADD_1_R___,
+        ___5ADD_2_L___,                                            ___5ADD_2_R___,
+        ___5ADD_3_L___,                                            ___5ADD_3_R___,
+        XXXXXXX, XXXXXXX,                                        XXXXXXX, XXXXXXX
     )
 };
 
-#ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI) },
-    [2] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
-};
-#endif
+bool achordion_chord_keymap(uint16_t tap_hold_keycode,
+                            keyrecord_t* tap_hold_record,
+                            uint16_t other_keycode,
+                            keyrecord_t* other_record) {
 
+  return tap_hold_record->event.key.row == 3;
+}
