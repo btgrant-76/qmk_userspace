aee1c6e1befa12ce75995efdf36369bd67b04a9b
Brian <brian@skeletonjack.net>
2024-07-15 18:59:54 -0500

    combo updates

--- users/btgrant-76/combos.h
+++ users/btgrant-76/combos.h
@@ -1,17 +1,17 @@
 const uint16_t PROGMEM QUOT[] = {L_ALT, SCLN_CTL, COMBO_END};
 const uint16_t PROGMEM D_QUOT[] = {LSFT_T(KC_F), RALT_T(KC_L), RCTL_T(KC_SCLN), COMBO_END};
-const uint16_t PROGMEM ESC_TOP[] = {KC_Q, KC_E, COMBO_END};
-const uint16_t PROGMEM ESC_BOT[] = {Z_HYPR, KC_X, COMBO_END};
-const uint16_t PROGMEM ESC_BOT2[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM ENTER[] = {HYPR_T(KC_SLSH), KC_DOT, COMBO_END};
-const uint16_t PROGMEM ENTER2[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM ESC[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM ESC_2[] = {Z_HYPR, KC_X, COMBO_END};
+const uint16_t PROGMEM ESC_3[] = {KC_BTN1, KC_BTN2, COMBO_END};
+const uint16_t PROGMEM ENTER[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM CAPS[] = {F_SFT, J_SFT, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(QUOT, KC_QUOT),
     COMBO(D_QUOT, KC_DQT),
-    COMBO(ESC_TOP, KC_ESC),
-    COMBO(ESC_BOT, KC_ESC),
-    COMBO(ESC_BOT2, KC_ESC),
+    COMBO(ESC, KC_ESC),
+    COMBO(ESC_2, KC_ESC),
+    COMBO(ESC_3, KC_ESC),
     COMBO(ENTER, KC_ENT),
-    COMBO(ENTER2, KC_ENT)
+    COMBO(CAPS, KC_CAPS)
 };
