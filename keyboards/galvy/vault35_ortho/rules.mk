6004e731f1b9b0ed131ccbc7fd3afafa4542142d
Brian <brian@skeletonjack.net>
2024-08-07 05:55:36 -0500

    Vault35 Ortho WIP

--- keyboards/galvy/vault35_ortho/rules.mk
+++ keyboards/galvy/vault35_ortho/rules.mk
@@ -1,15 +1,1 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = atmel-dfu
-
-# Build Options
-#   change yes to no to disable
-#
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = yes
-RGB_MATRIX_ENABLE = no
-LTO_ENABLE = yes
+# This file intentionally left blank
