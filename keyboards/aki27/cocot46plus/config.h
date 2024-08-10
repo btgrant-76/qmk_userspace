2f90578695e7461a367bbfeefb4b31b23b271501
Brian <brian@skeletonjack.net>
2024-08-10 10:23:13 -0500

    info.json migration

--- keyboards/aki27/cocot46plus/config.h
+++ keyboards/aki27/cocot46plus/config.h
@@ -17,52 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* key matrix size */
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 6
-
-// wiring of each half
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, NO_PIN }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#undef LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#undef LOCKING_RESYNC_ENABLE
-
 /* Limit the maximum layer number to 8 */
 #define LAYER_STATE_8BIT
-
-/* Rotary encoder */
-#define ENCODERS_PAD_A { D3 }
-#define ENCODERS_PAD_B { D2 }
-#define ENCODER_RESOLUTION 4
-
-#define TAPPING_TERM 200
-
-/* ws2812 RGB LED */
-#define WS2812_DI_PIN B6
-
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    //#define RGBLIGHT_ANIMATIONS
-    // select individual effect
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_TWINKLE
-
-    #define RGBLED_NUM 12
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
 
 #define ADNS5050_SCLK_PIN         B2
 #define ADNS5050_SDIO_PIN         B4
--- keyboards/galvy/vault35_ortho/config.h
+++ keyboards/galvy/vault35_ortho/config.h
@@ -3,34 +3,5 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* Define encoder pads */
-#define ENCODERS_PAD_A { B5, D1 }
-//#define ENCODERS_PAD_B { B6, D0 }
-
-/* Encoder resolution */
-#define ENCODER_RESOLUTION 4
-#define TAP_CODE_DELAY 10
-
 /* Set 0 if debouncing isn't needed */
 #define Debounce 0
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* Bootmagic Lite key configuration */
-//#define BOOTMAGIC_LITE_ROW 0
-//#define BOOTMAGIC_LITE_COLUMN 0
