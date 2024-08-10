ff8ae524d393db52dc24f569b16a2a84f0d58280
Brian <brian@skeletonjack.net>
2024-08-10 09:49:30 -0500

    misc updates

--- keyboards/galvy/vault35_ortho/config.h
+++ keyboards/galvy/vault35_ortho/config.h
@@ -3,13 +3,5 @@
 
 #pragma once
 
-/* Define encoder pads */
-// TODO make sure these work after being transferred to info.json
-#define ENCODERS_PAD_A { B5, D1 }
-//#define ENCODERS_PAD_B { B6, D0 }
-
-/* Encoder resolution */
-#define ENCODER_RESOLUTION 4
-
 /* Set 0 if debouncing isn't needed */
 #define Debounce 0
