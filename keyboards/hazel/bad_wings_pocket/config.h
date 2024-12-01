1bc7448376d3adcaacf7a47a3f1fd5995941a14e
Brian Grant <brian@skeletonjack.net>
2024-12-01 08:18:42 -0600

    Bad Wings Pocket

---
+++ keyboards/hazel/bad_wings_pocket/config.h
@@ -0,0 +1,5 @@
+// Copyright 2023 Jason Hazel (@jasonhazel)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
---
+++ keyboards/hazel/bad_wings_pocket/keymaps/btgrant-76/config.h
@@ -0,0 +1,15 @@
+// Copyright 2023 Jason Hazel (@jasonhazel)
+// Copyright 2024 Brian Grant (@btgrant-76)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+// TODO revisit these to see if any of them would work better than my "big keyboards configs"
+//#define TAPPING_TERM 200
+//#define PERMISSIVE_HOLD
+//#define IGNORE_MOD_TAP_INTERRUPT
+//#define TAPPING_FORCE_HOLD
+//#define TAPPING_TERM_PER_KEY
+//#define ONESHOT_TAP_TOGGLE 10
+//#define ONESHOT_TIMEOUT 500
+//#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
