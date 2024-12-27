2773223a33d33b60e8419f0b1d24ff18d0dfaf58
Brian Grant <brian@skeletonjack.net>
2024-12-27 10:29:14 -0600

    WIP

--- keyboards/mechwild/mercutio/keymaps/btgrant-76/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/btgrant-76/keymap.c
@@ -18,63 +18,79 @@
 #include QMK_KEYBOARD_H
 #include "btgrant-76.h"
 
-#ifdef COMBO_ENABLE
-#   include "combos.h"
-#endif
-
-#ifdef ENCODER_MAP_ENABLE
-#   include "encoder_map.h"
-#   include "encoder_press.h"
-#endif
+#include "combos.h"
+#include "encoder_map.h"
+#include "encoder_press.h" // TODO make sure all encoder presses are used below
+#include "tap_dances.c"
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_btgrant_all(
-                                                                                                                  ENC_BASE,
-      ___BASE_1_L___,               ___BASE_1_R___,
-      ___BASE_2_L___,               ___BASE_2_R___,
-      ___BASE_3_L___,               ___BASE_3_R___,
-      KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  KC_SPC,           KC_SPC,           KC_RALT, MO(2),            KC_RCTL ), // TODO space bars same as Le Chiffre
-
+                                                                             ENC_BASE,
+      ___BASE_1_L___,                                                  ___BASE_1_R___,
+      ___BASE_2_L___,                                                  ___BASE_2_R___,
+      ___BASE_3_L___,                                                  ___BASE_3_R___,
+      XXXXXXX, ESC_FUN, TAB_MSE,  BS_NUM, XXXXXXX, SPC_NAV, DEL_MSE, ENT_SYM, XXXXXXX
+  ),
   [_NUM] = LAYOUT_btgrant_all(
-                                                                                                                  ENC_NUM,
-      ___NUM_1_L___,               ___NUM_1_R___,
-      ___NUM_2_L___,               ___NUM_2_R___,
-      ___NUM_3_L___,               ___NUM_3_R___,
-  	  KC_TRNS, KC_TRNS, KC_TRNS,          KC_HOME, KC_TRNS,          KC_END,           KC_LEFT, KC_DOWN,          KC_RIGHT ),
-
+                                                                              ENC_NUM,
+      ___NUM_1_L___,                                                    ___NUM_1_R___,
+      ___NUM_2_L___,                                                    ___NUM_2_R___,
+      ___NUM_3_L___,                                                    ___NUM_3_R___,
+      _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, ZERO_TD, _______, _______
+  ),
   [_SYM] = LAYOUT_btgrant_all(
-                                                                                                                  ENC_SYM,
-      ___SYM_1_L___,               ___SYM_1_R___,
-      ___SYM_2_L___,               ___SYM_2_R___,
-      ___SYM_3_L___,               ___SYM_3_R___,
-  	  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS ),
-
+                                                                              ENC_SYM,
+      ___SYM_1_L___,                                                    ___SYM_1_R___,
+      ___SYM_2_L___,                                                    ___SYM_2_R___,
+      ___SYM_3_L___,                                                    ___SYM_3_R___,
+      _______, _______, KC_MUTE, KC_MPLY, XXXXXXX,LPRN_MSE, XXXXXXX, _______, _______
+  ),
   [_NAV] = LAYOUT_btgrant_all(
-                                                                                                                  ENC_NAV,
-      ___NAV_1_L___,               ___NAV_1_R___,
-      ___NAV_2_L___,               ___NAV_2_R___,
-      ___NAV_3_L___,               ___NAV_3_R___,
-  	  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS ),
-
+                                                                              ENC_NAV,
+      ___NAV_1_L___,                                                    ___NAV_1_R___,
+      ___NAV_2_L___,                                                    ___NAV_2_R___,
+      ___NAV_3_L___,                                                    ___NAV_3_R___,
+      _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______
+  ),
   [_FUN] = LAYOUT_btgrant_all(
-                                                                                                                  ENC_FUN,
-      ___FUN_1_L___,               ___FUN_1_R___,
-      ___FUN_2_L___,               ___FUN_2_R___,
-      ___FUN_3_L___,               ___FUN_3_R___,
-  	  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS ),
-
+                                                                              ENC_FUN,
+      ___FUN_1_L___,                                                    ___FUN_1_R___,
+      ___FUN_2_L___,                                                    ___FUN_2_R___,
+      ___FUN_3_L___,                                                    ___FUN_3_R___,
+      _______, _______, XXXXXXX, MO(_MSE),XXXXXXX,MO(_ADD), XXXXXXX, _______, _______
+  ),
   [_MSE] = LAYOUT_btgrant_all(
-                                                                                                                  ENC_MSE,
-      ___MSE_1_L___,               ___MSE_1_R___,
-      ___MSE_2_L___,               ___MSE_2_R___,
-      ___MSE_3_L___,               ___MSE_3_R___,
-  	  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS ),
-
+                                                                              ENC_MSE,
+      ___MSE_1_L___,                                                    ___MSE_1_R___,
+      ___MSE_2_L___,                                                    ___MSE_2_R___,
+      ___MSE_3_L___,                                                    ___MSE_3_R___,
+      _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______
+  ),
   [_ADD] = LAYOUT_btgrant_all(
-                                                                                                                  ENC_ADD,
-      XXXXXXX, ___5ADD_1_L___,               ___5ADD_1_R___, XXXXXXX,
-      XXXXXXX, ___5ADD_2_L___,               ___5ADD_2_R___, XXXXXXX,
-      XXXXXXX, ___5ADD_3_L___,               ___5ADD_3_R___, XXXXXXX,
-  	  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS )
+                                                                              ENC_ADD,
+      XXXXXXX, ___5ADD_1_L___,                                ___5ADD_1_R___, XXXXXXX,
+      XXXXXXX, ___5ADD_2_L___,                                ___5ADD_2_R___, XXXXXXX,
+      XXXXXXX, ___5ADD_3_L___,                                ___5ADD_3_R___, XXXXXXX,
+      _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______
+  ),
+//  ),
+//  [_NRM] = LAYOUT_btgrant_4x12( // TODO add a NRM layer
+//  //|-----------------------------------------------------|        |-----------------------------------------------------|
+//                                             ___NRM_1_L___,         ___NRM_1_R___,
+//  //|--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------|
+//                                             ___NRM_2_L___,         ___NRM_2_R___,
+//  //|--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------|
+//                                             ___NRM_3_L___,         ___NRM_3_R___,
+//  //|--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------|
+//      KC_LCTL, KC_LALT, KC_LGUI,         ___NRM_THUMB_L___,         ___NRM_THUMB_R___,          KC_RGUI, KC_RALT, KC_RCTL
+//  //|--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------|
+  [_NRM] = LAYOUT_btgrant_all(
+                                                                             ENC_BASE,
+      ___NRM_1_L___,                                                    ___NRM_1_R___,
+      ___NRM_2_L___,                                                    ___NRM_2_R___,
+      ___NRM_3_L___,                                                    ___NRM_3_R___,
+//      XXXXXXX, ESC_FUN, TAB_MSE,  BS_NUM, XXXXXXX, SPC_NAV, DEL_MSE, ENT_SYM, XXXXXXX
+      KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC,MO(_NUM), SPC_NAV, KC_RGUI, KC_RALT, KC_RCTL
+  )
 };
