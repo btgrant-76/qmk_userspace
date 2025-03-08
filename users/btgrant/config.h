9e4cd349293a8536591b917a3e311d8e336a60cb
Brian Grant <brian@skeletonjack.net>
2025-03-07 21:39:20 -0600

    update caps words config

--- users/btgrant/config.h
+++ users/btgrant/config.h
@@ -47,6 +47,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Caps Word
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_INVERT_ON_SHIFT
 #define CAPS_WORD_IDLE_TIMEOUT 2000  // 2 seconds
 
 // Auto Shift
