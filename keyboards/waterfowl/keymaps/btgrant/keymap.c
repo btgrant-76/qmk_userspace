5027f05025709ae073adcc8a5dbbc1e7668fa559
Brian Grant <brian@skeletonjack.net>
2026-03-29 11:27:07 -0500

    add QMK logo to Waterfowl OLED

--- keyboards/waterfowl/keymaps/btgrant/keymap.c
+++ keyboards/waterfowl/keymaps/btgrant/keymap.c
@@ -97,34 +97,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif
 
 #ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_180;
+    }
+    return OLED_ROTATION_0;
+}
 
 bool oled_task_user(void) {
     // Host Keyboard Layer Status
     if (is_keyboard_master()) {
-        oled_write_P(PSTR("LAYER:\n"), false);
+        static const char PROGMEM qmk_logo[] = {
+            0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+            0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+            0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
+        };
+        // clang-format on
+        oled_write_P(qmk_logo, false);
+
         oled_write_P(PSTR("\n"), false);
+        oled_write_P(PSTR("   LAYER:  "), false);
 
         switch (get_highest_layer(layer_state)) {
             case _BASE:
-                oled_write_P(PSTR("DEFAULT\n\n\n\n"), false);
+                oled_write_P(PSTR("DEFAULT   "), false);
                 break;
             case _NUM:
-                oled_write_P(PSTR("NUMBERS\n\n\n\n"), false);
+                oled_write_P(PSTR("NUMBERS   "), false);
                 break;
             case _SYM:
-                oled_write_P(PSTR("SYMBOLS\n\n\n\n"), false);
+                oled_write_P(PSTR("SYMBOLS   "), false);
                 break;
             case _FUN:
-                oled_write_P(PSTR("FUNCTION\n\n\n\n"), false);
+                oled_write_P(PSTR("FUNCTION  "), false);
                 break;
             case _NAV:
-                oled_write_P(PSTR("NAVIGATION\n\n\n\n"), false);
+                oled_write_P(PSTR("NAVIGATION"), false);
                 break;
             case _MSE:
-                oled_write_P(PSTR("MOUSE\n\n\n\n"), false);
+                oled_write_P(PSTR("MOUSE     "), false);
                 break;
             case _ADD:
-                oled_write_P(PSTR("ADDITIONAL\n\n\n\n"), false);
+                oled_write_P(PSTR("ADDITIONAL"), false);
                 break;
             default:
                 oled_write_ln_P(PSTR("Undefined"), false);
@@ -134,5 +148,4 @@ bool oled_task_user(void) {
         return true;
     }
 };
-
 #endif
