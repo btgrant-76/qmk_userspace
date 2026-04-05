bfd1ea1f51a74d08e58d8fef03f41304ca50b3b4
Brian Grant <brian@skeletonjack.net>
2026-04-04 22:39:18 -0500

    copy/paste from Sune's repo

---
+++ keyboard_defs/sune/rhymestone/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x51, 0x48, 0x35, 0xC7, 0x99, 0xA6, 0x67, 0x4B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {6, 6}
+#define VIAL_UNLOCK_COMBO_COLS {3, 1}
+
+#define MASTER_LEFT
+//#define MASTER_RIGHT
+
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_USB_DETECT
---
+++ keyboard_defs/sune/rhymestone/rules.mk
@@ -0,0 +1,6 @@
+#SERIAL_DRIVER = usart
+DEBOUNCE_TYPE = sym_defer_pk
+OLED_ENABLE = yes
+
+
+SRC += oled_helper.c
